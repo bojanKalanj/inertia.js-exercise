efca60203b79fee0c56f62be6ffed528c523006f
bojanNextesy <bojankalanj@nextesy.com>
2025-06-18 21:11:08 +0200

    Add authentication features

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_05_202916) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_18_183448) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status_id", null: false
     t.index ["status_id"], name: "index_projects_on_status_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "user_agent"
+    t.string "ip_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "statuses", force: :cascade do |t|
@@ -26,5 +35,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_05_202916) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.boolean "verified", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
   add_foreign_key "projects", "statuses"
+  add_foreign_key "sessions", "users"
 end
