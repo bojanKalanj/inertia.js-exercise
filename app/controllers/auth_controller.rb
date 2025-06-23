852fa93b354dd8723616b46c2aaa114d08dfe60b
bojanNextesy <bojankalanj@nextesy.com>
2025-06-23 20:11:55 +0200

    Change AuthController inheritance from ActionController::Base to ApplicationController for improved structure.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,4 +1,4 @@
-class AuthController < ActionController::Base
+class AuthController < ApplicationController
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
